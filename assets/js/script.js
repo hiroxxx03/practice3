39bb0ef054b8719c44f8a5a403f05a088dad0351
hiroxxx03 <115777709+hiroxxx03@users.noreply.github.com>
2022-11-17 11:56:22 +0900

    add guidance-img js

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,3 +17,32 @@ let menu = document.getElementById('menu-container');
 let guidance = document.querySelector('.guidance-text');
 
 button.addEventListener('click', hamburger);
+
+
+
+//                guidance-img
+
+let pics_src = new Array("mainvisual1","mainvisual2","mainvisual3");
+
+let num = 0;
+
+let img = document.querySelector(".guidance-img");
+
+let background = window.getComputedStyle(img).backgroundImage;
+
+num = 1;
+
+function slideshow1(){
+  if(num == 3) {
+    num = 0;
+    let replaceUrl = background.replace('mainvisual1', pics_src[num]);
+    img.style.backgroundImage = replaceUrl;
+    num++;
+  }else{
+    let replaceUrl = background.replace('mainvisual1', pics_src[num]);
+    img.style.backgroundImage = replaceUrl;
+    num++;
+  }
+}
+
+setInterval(slideshow1, 5000);
